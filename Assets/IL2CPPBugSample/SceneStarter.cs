07b9bb051e1c1db5d15d1706a621965c21199341
Kurai András <k.andras9642@gmail.com>
2020-10-30 22:31:35 +0100

    Add Hosting stuff

--- Assets/IL2CPPBugSample/SceneStarter.cs
+++ Assets/IL2CPPBugSample/SceneStarter.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class SceneStarter : MonoBehaviour
+namespace IL2CPPBugSample
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SceneStarter : MonoBehaviour
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
