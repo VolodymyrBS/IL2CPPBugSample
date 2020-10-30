6e97a3b64ad6379b55f512f58d8bc58e136712a1
Kurai András <k.andras9642@gmail.com>
2020-10-30 22:45:38 +0100

    Add hosting implementation stuff

--- Assets/IL2CPPBugSample/SceneStarter.cs
+++ Assets/IL2CPPBugSample/SceneStarter.cs
@@ -1,19 +1,59 @@
-﻿using UnityEngine;
+﻿using Injecter.Hosting.Unity;
+using Injecter.Unity;
+using MainThreadDispatcher;
+using MainThreadDispatcher.Unity;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Unity3D;
+using System;
+using UnityEngine;
 
 namespace IL2CPPBugSample
 {
-    public class SceneStarter : MonoBehaviour
+    [DefaultExecutionOrder(-999)]
+    public class SceneStarter : InjectStarter
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        private IHost _host;
 
+        protected override IServiceProvider CreateServiceProvider()
+        {
+            _host = new HostBuilder()
+                .UseUnityLifetime()
+                .ConfigureServices(ConfigureServices)
+                .UseSerilog((_, __, config) => config.WriteTo.Unity3D())
+                .Build();
+
+            return _host.Services;
         }
 
-        // Update is called once per frame
-        void Update()
+        protected override void Awake()
         {
+            base.Awake();
 
+            LoadEagerServices();
+
+            _host.Start();
+        }
+
+        private void OnApplicationQuit() => _host?.Dispose();
+
+        private void LoadEagerServices() => _host.Services.GetRequiredService<IMainThreadDispatcher>();
+
+        private static void ConfigureServices(HostBuilderContext _, IServiceCollection services)
+        {
+            services.AddSceneInjector(
+                options => options.UseCaching = true,
+                options =>
+                {
+                    options.DontDestroyOnLoad = true;
+                    options.InjectionBehavior = SceneInjectorOptions.Behavior.Factory;
+                });
+
+            services.AddSingleton<IMainThreadDispatcher>(sp => FindObjectOfType<UnityMainThreadDispatcher>());
+
+            services.AddMediatR(typeof(SceneStarter).Assembly);
         }
     }
 }
